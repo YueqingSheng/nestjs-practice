4ce1e0ef4a3be4bb4ca298ca8bcc4b5dbd14c4cd
Yueqing Sheng <shengyueqing@gmail.com>
2019-12-20 04:01:15 +0800

    reimport

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { CommonModule } from './modules/common/common.module';
+import { CoreModule } from './modules/core/core.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule, CommonModule, CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
